19982aae15b93e7135ea0fc135bb330ca4a09316
Rachel K <raech.kanati@gmail.com>
2019-12-19 02:07:00 +0000

    wip

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -36,18 +36,26 @@ impl Edge {
 type Nodes = HashMap<NodeID, Tile>;
 type Edges = HashSet<Edge>;
 
-fn extract_graph(lines: &[&str]) -> (Nodes, Edges) {
+struct AdjNode {
+    pub tile:      Tile,
+    pub neighbors: HashSet<(NodeID, i32)>
+}
+
+type Adjs  = HashMap<NodeID, AdjNode>;
+
+fn extract_graph(lines: &[&str]) -> (Nodes, Edges, NodeID) {
     let mut nodes = Nodes::new();
     let mut edges = Edges::new();
+    let mut start = None;
 
     let mut id = 0;
 
-    let mut prev_row = Vec::new();
+    let mut prev_row: Vec<Option<NodeID>> = Vec::new();
 
     for line in lines.iter() {
         let mut this_row = Vec::new();
 
-        for (ch, above) in line.chars().zip(prev_row.iter()) {
+        for (ch, above) in line.chars().zip(prev_row.into_iter().chain(std::iter::repeat(None))) {
             let tile = match ch {
                 '#' => {
                     this_row.push(None);
@@ -57,7 +65,8 @@ fn extract_graph(lines: &[&str]) -> (Nodes, Edges) {
                 c if c.is_ascii_lowercase() => Tile::Key(c),
                 c if c.is_ascii_uppercase() => Tile::Door(c),
                 '@' => {
-                    // TODO record start pos
+                    assert!(start.is_none());
+                    start = Some(id);
                     Tile::Start
                 }
                 _ => panic!()
@@ -70,7 +79,7 @@ fn extract_graph(lines: &[&str]) -> (Nodes, Edges) {
             }
 
             if let Some(above_id) = above {
-                edges.insert(Edge::new(id, *above_id, 1));
+                edges.insert(Edge::new(id, above_id, 1));
             }
 
             id += 1;
@@ -80,7 +89,7 @@ fn extract_graph(lines: &[&str]) -> (Nodes, Edges) {
         prev_row = this_row;
     }
 
-    (nodes, edges)
+    (nodes, edges, start.unwrap())
 }
 
 fn prune_graph(mut nodes: Nodes, mut edges: Edges) -> (Nodes, Edges) {
@@ -134,46 +143,121 @@ fn prune_graph(mut nodes: Nodes, mut edges: Edges) -> (Nodes, Edges) {
     (nodes, edges)
 }
 
-fn simplify_graph(mut nodes: Nodes, edges: Edges) -> (Nodes, Edges) {
-    // find edges incident to degree-2 nodes
-    let mut incidents: HashMap<NodeID, Option<Edges>> = HashMap::new();
+fn simplify_graph(mut nodes: Nodes, mut edges: Edges) -> (Nodes, Edges) {
+    // remove uninteresting nodes - makes merging edges easier
+    nodes = nodes.into_iter()
+        .filter_map(|(id, tile)| match tile {
+            Tile::Clear => None,
+            _           => Some((id, tile))
+        })
+        .collect();
 
-    for edge in edges.iter() {
-        use std::collections::hash_map::Entry::*;
-        match incidents.entry(edge.min) {
-            Occupied(mut entry) => {
-                if let Some(set) = entry.get_mut() {
-                    if set.len() < 2 {
-                        set.insert(*edge);
-                    }
-                    else {
-                        entry.insert(None);
-                    }
-                }
-            }
-            Vacant(entry) => {
-                let mut new_set = Edges::new();
-                new_set.insert(*edge);
-                entry.insert(Some(new_set));
-            }
+    for edge in edges {
+
+    }
+
+    (nodes, edges)
+}
+
+fn to_adjacency(nodes: Nodes, edges: Edges) -> Adjs {
+    let mut adjs: Adjs = nodes.into_iter()
+        .map(|(id, tile)| (id, AdjNode { tile: tile, neighbors: HashSet::new() }))
+        .collect();
+
+    for edge in edges.into_iter() {
+        adjs.get_mut(&edge.min).unwrap().neighbors.insert((edge.max, edge.weight));
+        adjs.get_mut(&edge.max).unwrap().neighbors.insert((edge.min, edge.weight));
+    }
+
+    adjs
+}
+
+#[derive(Clone, Copy, Debug)]
+struct KeyBits(u32);
+
+impl KeyBits {
+    fn key_bit(key: char) -> u32 {
+        let key = key.to_ascii_lowercase();
+        match key {
+            'a'..='z' => 1 << (u32::from(key) - u32::from('a')),
+            _         => panic!("bad key {}", key)
         }
     }
 
-    let incidents: HashMap<NodeID, Edges> = incidents.into_iter()
-        .filter_map(|(id, edges)| edges.map(move |edges| (id, edges)))
-        .collect();
+    fn none() -> KeyBits { KeyBits(0) }
 
-    for edge in incidents.values().flatten() {
+    fn with(self, key: char) -> KeyBits {
+        KeyBits(self.0 | Self::key_bit(key))
     }
 
+    fn has(self, key: char) -> bool {
+        self.0 & Self::key_bit(key) != 0
+    }
 
-    (nodes, edges)
+    fn has_all(self) -> bool {
+        self.0 == 1 << 26 - 1
+    }
+}
+
+fn search(
+    adjs: &Adjs,
+    keys: KeyBits,
+    cur:  NodeID,
+    prev: Option<NodeID>,
+    cost: i32,
+    best: &mut i32,
+    ub:   i32)
+{
+    dbg!(cur, *best);
+
+    let node = &adjs[&cur];
+
+    let keys = match node.tile {
+        Tile::Key(ch) => keys.with(ch),
+        Tile::Door(ch) if !keys.has(ch) => { return; }
+        _ => keys
+    };
+
+    if keys.has_all() {
+        *best = std::cmp::min(*best, cost);
+        return;
+    }
+
+    for (neighbor, delta) in node.neighbors.iter() {
+        if cost + delta > ub {
+            continue;
+        }
+
+        // don't backtrack if we have a choice
+        if let Some(prev_id) = prev {
+            if node.neighbors.len() > 1 && *neighbor == prev_id {
+                continue;
+            }
+        }
+
+        // don't bother continuing this path if we have a better one
+        let new_cost = cost + delta;
+        if *best <= new_cost {
+            continue;
+        }
+
+        search(adjs, keys, *neighbor, Some(cur), new_cost, best, ub);
+    }
+}
+
+fn shortest_tour(adjs: &Adjs, start: NodeID, upper_bound: i32) -> i32 {
+    let mut best = std::i32::MAX;
+    search(adjs, KeyBits::none(), start, None, 0, &mut best, upper_bound);
+    best
 }
 
 fn main() {
     let input = include_str!("../input");
     let lines: Vec<&str> = input.lines().collect();
-    let (nodes, edges) = extract_graph(&lines);
+    let (nodes, edges, start) = extract_graph(&lines);
     let (nodes, edges) = prune_graph(nodes, edges);
+    let upper_bound: i32 = edges.iter().map(|edge| edge.weight).sum();
+    let adjs = to_adjacency(nodes, edges);
+    let len = shortest_tour(&adjs, start, upper_bound);
 }
 
